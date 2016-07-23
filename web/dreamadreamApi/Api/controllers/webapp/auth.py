c817f91308b2cce7b039497312a67f990ba72c84
Kshitij <kshitij@hxworld.xyz>
2016-07-23 18:37:17 +0530

    add login

--- web/dreamadreamApi/Api/controllers/webapp/auth.py
+++ web/dreamadreamApi/Api/controllers/webapp/auth.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -15,12 +16,24 @@ def register(request):
     try:
         Member.objects.get(email_id=user.email_id)
         User.objects.get(username=user.email_id)
-    except Member.DoesNotExist:
+    except (Member.DoesNotExist, User.DoesNotExist):
         return Response(data="User Already exists please try again")
     else:
-        user = _register(user=user)
+        user = _register(request=request)
 
     return Response(data=user, status=status.HTTP_202_ACCEPTED)
+
+
+def login(request):
+    user = request.data.get('user', None)
+    try:
+        Member.objects.get(email_id=user.email_id)
+        User.objects.get(username=user.email_id)
+    except (Member.DoesNotExist, User.DoesNotExist):
+        return Response(data="User Not registered")
+    else:
+        user = authenticate(username=user.email_id, password=user.password)
+        return Response(data=user)
 
 
 def _register(request):
