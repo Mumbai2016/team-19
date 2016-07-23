94f9417720e27bfb9d1e4903ff3c9c8bfe937a56
racerronny <tronak19@gmail.com>
2016-07-23 20:25:20 +0530

    fix merge

--- web/dreamadreamApi/Api/models.py
+++ web/dreamadreamApi/Api/models.py
@@ -1,6 +1,32 @@
 from django.db import models
 
 
+class Call(models.Model):
+    member = models.ManyToManyField(Member, related_name="call_member", default=None)
+    call_date = models.CharField(max_length=20)
+    status_at_date = models.CharField(max_length=255)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    qualification = models.CharField(max_length=255)
+    age_min = models.DecimalField(max_digits=2, decimal_places=0)
+    age_max = models.DecimalField(max_digits=2, decimal_places=0)
+
+
+class EventRegistration(models.Model):
+    member = models.ForeignKey(Member, related_name="registered_member")
+    registered_event = models.ForeignKey(Event, related_name="registered_event")
+
+
+class Feedback(models.Model):
+    member = models.ForeignKey(Member, related_name="feedback_member")
+    event = models.ForeignKey(Event, related_name="feedback_event")
+    feedback = models.CharField(max_length=255)
+
+
 class Member(models.Model):
     status = models.ForeignKey(Status, related_name="member_status")
     full_name = models.CharField(max_length=100)
@@ -33,21 +59,6 @@ class Member(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
-class Event(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    qualification = models.CharField(max_length=255)
-    age_min = models.DecimalField(max_digits=2, decimal_places=0)
-    age_max = models.DecimalField(max_digits=2, decimal_places=0)
-
-
-class Call(models.Model):
-    member = models.ManyToManyField(Member, related_name="call_member", default=None)
-    call_date = models.CharField(max_length=20)
-    status_at_date = models.CharField(max_length=255)
 
 
 class Request(models.Model):
