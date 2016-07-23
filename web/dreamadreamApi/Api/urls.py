2db6b0a270b834af5c7338ea0b4fbb30efae645d
Kshitij <kshitij@hxworld.xyz>
2016-07-24 04:33:32 +0530

    exotel url

--- web/dreamadreamApi/Api/urls.py
+++ web/dreamadreamApi/Api/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^webapp/member/list', 'Api.controllers.webapp.member.list', name='list_members'),
     url(r'^webapp/member/get/(?P<id>[0-9]+$)', 'Api.controllers.admin.members.member_detail', name='member_detail'),
     url(r'^webapp/member/delete/(?P<id>[0-9]+$)', 'Api.controllers.admin.members.member_delete', name='member_delete'),
+
+    url(r'exotel/make_exotel_call/$',
+        'Api.controllers.admin.exotelcalls.make_exotel_call_task', name='make_exotel_calls'),
 ]
