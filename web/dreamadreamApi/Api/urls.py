cffa9f5a0dc758af68b6e98c5e86d157bdbf910b
racerronny <tronak19@gmail.com>
2016-07-24 04:16:48 +0530

    Events

--- web/dreamadreamApi/Api/urls.py
+++ web/dreamadreamApi/Api/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     url(r'^webapp/member/list', 'Api.controllers.webapp.member.list', name='list_members'),
     url(r'^webapp/member/get/(?P<id>[0-9]+$)', 'Api.controllers.admin.members.member_detail', name='member_detail'),
     url(r'^webapp/member/delete/(?P<id>[0-9]+$)', 'Api.controllers.admin.members.member_delete', name='member_delete'),
+
+    url(r'^webapp/event/create', 'Api.controllers.admin.event.event_create', name='event_create'),
+    url(r'^webapp/event/list', 'Api.controllers.admin.event.event_list', name='event_list'),
+    url(r'^webapp/event/delete/(?P<id>[0-9]+$)', 'Api.controllers.admin.event.event_delete', name='event_delete'),
 ]
