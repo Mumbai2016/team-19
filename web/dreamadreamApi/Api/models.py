f64f8924322f351ed0c36aebac89496a5e9ca181
racerronny <tronak19@gmail.com>
2016-07-23 17:50:45 +0530

    Models

--- web/dreamadreamApi/Api/models.py
+++ web/dreamadreamApi/Api/models.py
@@ -5,6 +5,8 @@ class Member(models.Model):
     full_name = models.CharField(max_length=100)
     email_id = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=32)
+    address = models.CharField(max_length=255)
+
 
     gender = models.CharField(max_length=10)
     dob = models.CharField(max_length=20)
@@ -14,6 +16,10 @@ class Member(models.Model):
     mother_mobile_no = models.CharField(max_length=14, blank=True, null=True)
     father_mobile_no = models.CharField(max_length=14, blank=True, null=True)
     other_no = models.CharField(max_length=14, blank=True, null=True)
+
+    qualification = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    goal = models.CharField(max_length=255)
 
     program_start_date = models.CharField(max_length=20, blank=True, null=True)
     program_duration = models.CharField(max_length=20, blank=True, null=True)
@@ -26,4 +32,31 @@ class Member(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    qualification = models.CharField(max_length=255)
+    age_min = models.DecimalField(max_digits=2, decimal_places=0)
+    age_max = models.DecimalField(max_digits=2, decimal_places=0)
+
+
+class Call(models.Model):
+    member = models.ManyToManyField(Member, related_name="member", null=True, default=None)
+    call_date = models.CharField(max_length=20)
+    status_at_date = models.CharField(max_length=255)
+
+
+class Request(models.Model):
+    member = models.ManyToManyRel(Member, related_name="member", null=True, default=None)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+
+class Event_Registration(models.Model):
+    member = models.ForeignKey(Member, related_name="registered_member")
+    registered_event = models.ForeignKey(Event, related_name="registered_event")
+
 
